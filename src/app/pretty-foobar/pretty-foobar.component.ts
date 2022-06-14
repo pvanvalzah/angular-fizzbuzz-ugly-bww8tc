c5f6baadc19fb747670ebde6cfc0e81543f5e220
pvanvalzah <pvanvalzah@millonex.com>
2022-06-14 13:34:48 -0500

    1 file modified

--- src/app/pretty-foobar/pretty-foobar.component.ts
+++ src/app/pretty-foobar/pretty-foobar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatButton } from '@angular/material';
 
 @Component({
-  selector: 'app-fizzbuzz',
+  selector: 'app-foobar',
   templateUrl: './pretty-foobar.component.html',
   styleUrls: ['./pretty-foobar.component.css'],
 })
